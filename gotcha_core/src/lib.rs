f2a6b7492051a987409ddff68585f7ca19dfe8b6
Kilerd Chan <blove694@gmail.com>
2023-02-20 13:24:23 +0800

    refact: remove parameter generator from api object

--- gotcha_core/src/lib.rs
+++ gotcha_core/src/lib.rs
@@ -1,6 +1,6 @@
 use convert_case::{Case, Casing};
 use http::Method;
-use oas::{Operation, Parameter, Referenceable, Response, Responses,ParameterIn,Schema};
+use oas::{Operation, Parameter, Referenceable, Response, Responses, ParameterIn, Schema};
 use std::collections::{BTreeMap};
 use actix_web::web::{Data, Path};
 
@@ -50,43 +50,16 @@ pub trait ApiObject {
     fn name() -> &'static str;
     fn required() -> bool;
     fn type_() -> &'static str;
-    fn generate() -> Parameter {
-        Parameter {
-            name: Self::name().to_string(),
-            _in: ParameterIn::Path,
-            description: None,
-            required: Some(Self::required()),
-            deprecated: Some(false),
-            allow_empty_value: None,
-            style: None,
-            explode: None,
-            allow_reserved:None,
-            schema: Some(oas::Referenceable::Data(Schema{
-                _type: Some(Self::type_().to_string()),
-                format:None,
-                nullable:None,
-                extras:Default::default()
-            })),
-            example: None,
-            examples: None,
-            content:None
-        }
-        // Parameter {
-        //     name:
-        //     _in: ParameterIn::Query,
-
-        // }
-
-    }
     fn generate_schema() -> Schema {
-        Schema{
+        Schema {
             _type: Some(Self::type_().to_string()),
-            format:None,
-            nullable:None,
-            extras:Default::default()
+            format: None,
+            nullable: None,
+            extras: Default::default(),
         }
     }
 }
+
 pub trait ParameterProvider {
     fn location() -> ParameterIn;
     fn generate(url: String) -> Option<Vec<Parameter>>;
@@ -108,23 +81,22 @@ macro_rules! impl_primitive_type {
     }
 }
 
-impl_primitive_type!{ i8, "i32", "number"}
-impl_primitive_type!{ i16, "i16", "number"}
-impl_primitive_type!{ i32, "i32", "number"}
-impl_primitive_type!{ i64, "i64", "number"}
-impl_primitive_type!{ isize, "isize", "number"}
-impl_primitive_type!{ u8, "u8", "number"}
-impl_primitive_type!{ u16, "u16", "number"}
-impl_primitive_type!{ u32, "u32", "number"}
-impl_primitive_type!{ u64, "u64", "number"}
-impl_primitive_type!{ usize, "usize", "number"}
-impl_primitive_type!{ String, "string", "string"}
+impl_primitive_type! { i8, "i32", "number"}
+impl_primitive_type! { i16, "i16", "number"}
+impl_primitive_type! { i32, "i32", "number"}
+impl_primitive_type! { i64, "i64", "number"}
+impl_primitive_type! { isize, "isize", "number"}
+impl_primitive_type! { u8, "u8", "number"}
+impl_primitive_type! { u16, "u16", "number"}
+impl_primitive_type! { u32, "u32", "number"}
+impl_primitive_type! { u64, "u64", "number"}
+impl_primitive_type! { usize, "usize", "number"}
+impl_primitive_type! { String, "string", "string"}
 
 
 
 
 impl<T: ApiObject> ApiObject for Option<T> {
-
     fn name() -> &'static str {
         T::name()
     }
@@ -136,47 +108,71 @@ impl<T: ApiObject> ApiObject for Option<T> {
     fn type_() -> &'static str {
         T::type_()
     }
+    fn generate_schema() -> Schema {
+        T::generate_schema()
+    }
 }
 
-impl<T1:ApiObject> ParameterProvider for Path<(T1, )> {
+fn build_param(name: String, _in: ParameterIn, required: bool, schema: Schema) -> Parameter {
+    Parameter {
+        name,
+        _in,
+        description: None,
+        required: Some(required),
+        deprecated: None,
+        allow_empty_value: None,
+        style: None,
+        explode: None,
+        allow_reserved: None,
+        schema: Some(Referenceable::Data(schema)),
+        example: None,
+        examples: None,
+        content: None,
+    }
+}
 
-
+impl<T1: ApiObject> ParameterProvider for Path<(T1, )> {
     fn location() -> ParameterIn {
         ParameterIn::Path
     }
     fn generate(url: String) -> Option<Vec<Parameter>> {
         let pattern = regex::Regex::new(r"\{([^\}]+)\}").unwrap();
         let param_names_in_path: Vec<String> = pattern.captures_iter(&url)
-        .map(|digits| digits.get(1).unwrap().as_str().to_string())
-        .collect();
+            .map(|digits| digits.get(1).unwrap().as_str().to_string())
+            .collect();
 
-        let mut t1_param = T1::generate();
-        t1_param._in = Self::location();
-        t1_param.name = param_names_in_path.get(0).cloned().expect("cannot get param in path");
-
+        let t1_param = build_param(
+            param_names_in_path.get(0).cloned().expect("cannot get param in path"),
+            ParameterIn::Path,
+            T1::required(),
+            T1::generate_schema(),
+        );
         Some(vec![t1_param])
     }
 }
 
-impl<T1:ApiObject, T2: ApiObject> ParameterProvider for Path<(T1, T2)> {
-
+impl<T1: ApiObject, T2: ApiObject> ParameterProvider for Path<(T1, T2)> {
     fn location() -> ParameterIn {
         ParameterIn::Path
     }
     fn generate(url: String) -> Option<Vec<Parameter>> {
         let pattern = regex::Regex::new(r"\{([^\}]+)\}").unwrap();
         let param_names_in_path: Vec<String> = pattern.captures_iter(&url)
-        .map(|digits| digits.get(1).unwrap().as_str().to_string())
-        .collect();
+            .map(|digits| digits.get(1).unwrap().as_str().to_string())
+            .collect();
 
-        let mut t1_param = T1::generate();
-        t1_param._in = Self::location();
-        t1_param.name = param_names_in_path.get(0).cloned().expect("cannot get param in path");
-
-
-        let mut t2_param = T2::generate();
-        t2_param._in = Self::location();
-        t2_param.name = param_names_in_path.get(1).cloned().expect("cannot get param in path");
+        let t1_param = build_param(
+            param_names_in_path.get(0).cloned().expect("cannot get param in path"),
+            ParameterIn::Path,
+            T1::required(),
+            T1::generate_schema(),
+        );
+        let t2_param = build_param(
+            param_names_in_path.get(1).cloned().expect("cannot get param in path"),
+            ParameterIn::Path,
+            T2::required(),
+            T2::generate_schema(),
+        );
 
         Some(vec![t1_param, t2_param])
     }
@@ -190,31 +186,17 @@ impl<T: ApiObject> ParameterProvider for Path<T> {
         let mut ret = vec![];
         let mut schema = T::generate_schema();
         if let Some(mut properties) = schema.extras.remove("properties") {
-            if let Some(properties ) = properties.as_object_mut() {
-                properties.iter_mut().for_each(|(key,value)| {
+            if let Some(properties) = properties.as_object_mut() {
+                properties.iter_mut().for_each(|(key, value)| {
                     let schema = serde_json::from_value(value.clone()).unwrap();
-                    ret.push(Parameter {
-                        name: key.to_string(),
-                        _in: ParameterIn::Path,
-                        description: None,
-                        required: Some(T::required()),
-                        deprecated: Some(false),
-                        allow_empty_value: None,
-                        style: None,
-                        explode: None,
-                        allow_reserved:None,
-                        schema: Some(oas::Referenceable::Data(schema)),
-                        example: None,
-                        examples: None,
-                        content:None
-                    })
+                    let param = build_param(key.to_string(), ParameterIn::Path, T::required(), schema);
+                    ret.push(param);
                 })
             }
         }
         Some(ret)
     }
 }
-
 
 impl<T> ParameterProvider for Data<T> {
     fn location() -> ParameterIn {
