1debb378c91e015e7acbbbc5ea67a610e7fd4f20
Kilerd Chan <blove694@gmail.com>
2023-10-07 14:30:57 +0800

    feat: #15 store non-pk fields in static array

--- conservator_macro/src/domain.rs
+++ conservator_macro/src/domain.rs
@@ -1,7 +1,7 @@
 use darling::{FromDeriveInput, FromField};
 use itertools::Itertools;
 use proc_macro2::Span;
-use quote::quote;
+use quote::{quote, format_ident};
 use syn::spanned::Spanned;
 use syn::{parse2, DeriveInput};
 
@@ -38,6 +38,8 @@ pub(crate) fn handler(input: proc_macro2::TokenStream) -> Result<proc_macro2::To
     let crud_opts: DomainOpts = DomainOpts::from_derive_input(&x1).unwrap();
 
     let fields = crud_opts.data.take_struct().unwrap();
+    let non_pk_field_names = fields.fields.iter().filter(|field| field.primary_key.is_none()).filter_map(|field|field.ident.as_ref().map(|it|it.to_string())).map(|it| quote!{#it}).collect_vec();
+    let non_pk_fields_count = non_pk_field_names.len();
     let mut pk_count = fields.fields.into_iter().filter(|field| field.primary_key == Some(true)).collect_vec();
 
     let pk_field = match pk_count.len() {
@@ -54,12 +56,16 @@ pub(crate) fn handler(input: proc_macro2::TokenStream) -> Result<proc_macro2::To
 
     let table_name = &crud_opts.table;
     let ident = crud_opts.ident;
+    let field_ident = format_ident!("__CONSERVATOR_{}_DOMAIN_FIELDS", ident.clone().to_string().to_uppercase());
 
     let find_by_id_sql = find_by_id(&crud_opts.table, &pk_field_name);
     let fetch_all_sql = fetch_all(&crud_opts.table);
     let delete_by_pk = delete_by_pk(&crud_opts.table, &pk_field_name);
 
     Ok(quote! {
+
+
+        static #field_ident : [&'static; #non_pk_fields_count] = [#(#non_pk_field_names ,)*];
         #[async_trait::async_trait]
         impl ::conservator::Domain for #ident {
             const PK_FIELD_NAME: &'static str = #pk_field_name;
@@ -130,6 +136,8 @@ mod test {
             }
         };
         let expected_output = quote! {
+
+            static __CONSERVATOR_USERENTITY_DOMAIN_FIELDS :[&'static; 6usize] = ["username" , "email" , "password" , "role" , "create_at" , "last_login_at" ,];
             #[async_trait::async_trait]
             impl ::conservator::Domain for UserEntity {
                 const PK_FIELD_NAME: &'static str = "id";
